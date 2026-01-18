c5af33a26fec702c91150128b461b70513fd87dc
Sayyam Jain <143936791+Sayyamjain23@users.noreply.github.com>
2026-01-18 22:07:14 +0530

    Change SameSite attribute of cookie to 'none'

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,7 +79,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
